0b4ef6b282e9b814f5030776a18e53f0b2a2283c
EmpDucks <142122298+EmpDucks@users.noreply.github.com>
2024-05-08 13:12:43 -0600

    Update group_project_probably.py

--- group_project_probably.py
+++ group_project_probably.py
@@ -1,6 +1,6 @@
 radius = float(input("What is the radius of the circle? (Type numbers only, don't include a measurement)"))
 # the "float" function converts the input into a floating-point number (or a number with a decimal) used to convert numbers and strings
 area = radius * int(3.141519)
-# This line is used to calculate the are. it calculates the inputted number by pi.
+# This line is used to calculate the area. it calculates the inputted number by pi.
 print(f"The area of the circle is ~{area}")
 # print results.
